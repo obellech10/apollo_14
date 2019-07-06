8968b92526fca0b95a81fba20ae744dbee50b341
Oscar Bellechasse <obellech10@gmail.com>
2019-07-06 12:43:32 -0600

    Add has many relation and method for average age

--- app/models/astronaut.rb
+++ app/models/astronaut.rb
@@ -1,3 +1,15 @@
 class Astronaut < ApplicationRecord
   validates_presence_of :name, :age, :job
+  has_many :astronaut_missions
+  has_many :missions
+
+  def self.average_age
+    @astros = Astronaut.all
+    age = []
+    @astros.map do |astro|
+      age << astro.age
+    end
+    age.sum / @astros.count
+  end
+
 end
