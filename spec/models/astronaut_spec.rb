edd31eebd16534eaa541ec2820820fbf8767e165
Oscar Bellechasse <obellech10@gmail.com>
2019-07-06 12:44:53 -0600

    Create tests for astro index page

--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -11,4 +11,14 @@ describe Astronaut, type: :model do
     it { should have_many :astronaut_missions}
     it { should have_many :missions}
   end
+
+  it 'I see average age of all astronauts' do
+    astro_1 = Astronaut.create!(name: "Neil Armstrong", age: 37, job: "Commander")
+    astro_2 = Astronaut.create!(name: "Buzz Aldrin", age: 35, job: "Lunar Pilot")
+    astro_3 = Astronaut.create!(name: "Alan Shepard", age: 33, job: "Module Pilot")
+
+    visit '/astronauts'
+
+    expect(page).to have_content("Average Age: 35")
+  end
 end
